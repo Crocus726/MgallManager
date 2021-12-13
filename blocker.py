61fa19906b2e55ee9b521f9d9a433e8730335f3b
Gloriel621 <gloriel621@gmail.com>
2021-12-13 11:53:47 +0900

    Update blocker.py

--- blocker.py
+++ blocker.py
@@ -1,13 +1,20 @@
 import requests
 
-def blocker(session: requests.Session):
+class blocker:
+    def __init__():
+        pass
 
-    url = "https://gall.dcinside.com/ajax/managements_ajax/update_ipblock"
-    session.headers["Referer"] = "https://gall.dcinside.com/mgallery/management/gallery?id=pessimism"
-    a = session.cookies['ci_c']
+
+def blocker(session: requests.Session, gall_id):
+
+    # 해당 갤러리로 이동
+    BASE_URL = "https://gall.dcinside.com/mgallery/board/lists/"
+    params = {'id': gall_id }
+    response = session.get(BASE_URL, params=params)
+
     post_data = {
         "ci_t": session.cookies['ci_c'],
-        "gallery_id": "pessimism",
+        "gallery_id": gall_id,
         "_GALLTYPE_": "M",
         "proxy_time": 2880,
         "mobile_time": 60,
@@ -16,6 +23,7 @@ def blocker(session: requests.Session):
         "img_block_use": -1,
         "img_block_time": None
     }
+
     texts = ["proxy_time", "mobile_time"]
     if post_data["proxy_time"] > 0:
             post_data["proxy_use"] = 1
@@ -27,9 +35,12 @@ def blocker(session: requests.Session):
     else:
         post_data["mobile_use"] = 0
 
+    url = "https://gall.dcinside.com/ajax/managements_ajax/update_ipblock"
     response = session.post(url, data=post_data)
-    if "fail" in response.text:
-        print("Cannot manage gallery settings.")
-    elif "success" in response.text:
+
+    if "success" in response.text:
         print(f"vpn 차단 : {post_data[texts[0]]//60}시간", end = ", ")
         print(f"통신사 IP 차단 : {post_data[texts[1]]}분")
+    
+    else:
+        print("Cannot manage gallery settings.")
