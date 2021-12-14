a285cf5c3d56d0af7165deefe9ae2900c0034e7c
Gloriel621 <gloriel621@gmail.com>
2021-12-14 11:58:40 +0900

    아이디/비밀번호를 파일로 관리

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import sys
 import time
 import schedule
-from datetime import datetime
 
-from utils import login, logout
+from utils import login, logout, auth
 from blocker import Blocker
 from deleter import Deleter
 
 
-def main(user_id, user_pw, gall_id):
+def blocker(user_id, user_pw, gall_id):
 
-    print(datetime.now())
     session = login(user_id, user_pw)
     blocker = Blocker(session, gall_id)
-    deleter = Deleter(session, gall_id)
     blocker.block()
+    logout(session)
+
+def deleter(user_id, user_pw, gall_id):
+
+    session = login(user_id, user_pw)
+    deleter = Deleter(session, gall_id)
     deleter.delete()
     logout(session)
 
@@ -22,11 +25,16 @@ def main(user_id, user_pw, gall_id):
 if __name__ == "__main__":
 
     gall_id = sys.argv[1]
-    user_id = sys.argv[2]
-    user_pw = sys.argv[3]
+    mode = sys.argv[2]
+    user_id, user_pw = auth()
 
-    main(user_id, user_pw, gall_id)
-    schedule.every(59).minutes.do(main, user_id, user_pw, gall_id)
+    if mode == "block":
+        blocker(user_id, user_pw, gall_id)
+        schedule.every(59).minutes.do(blocker, user_id, user_pw, gall_id)
+
+    elif mode == "delete":
+        deleter(user_id, user_pw, gall_id)
+        schedule.every(1).minutes.do(deleter, user_id, user_pw, gall_id)
 
     while True:
         schedule.run_pending()
