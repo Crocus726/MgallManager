5490c3c4ea76cc602f9c92804f272d330e25c687
Gloriel621 <gloriel621@gmail.com>
2021-12-16 12:35:46 +0900

    GUI 실행

--- main.py
+++ main.py
@@ -1,41 +1,12 @@
+from PyQt5.QtWidgets import QApplication
 import sys
-import time
-import schedule
+from gui import MgallManager
 
-from utils import login, logout, auth
-from blocker import Blocker
-from deleter import Deleter
-
-
-def blocker(user_id, user_pw, gall_id):
-
-    session = login(user_id, user_pw)
-    blocker = Blocker(session, gall_id)
-    blocker.block()
-    logout(session)
-
-def deleter(user_id, user_pw, gall_id):
-
-    session = login(user_id, user_pw)
-    deleter = Deleter(session, gall_id)
-    deleter.delete()
-    logout(session)
+def main():
+    app = QApplication(sys.argv)
+    ex = MgallManager()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
-
-    gall_id = sys.argv[1]
-    mode = sys.argv[2]
-    user_id, user_pw = auth()
-
-    if mode == "block":
-        blocker(user_id, user_pw, gall_id)
-        schedule.every(59).minutes.do(blocker, user_id, user_pw, gall_id)
-
-    elif mode == "delete":
-        deleter(user_id, user_pw, gall_id)
-        schedule.every(1).minutes.do(deleter, user_id, user_pw, gall_id)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(10)
+    main()
