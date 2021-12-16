c1f1e6ac25faebf8a1978dc364a6f770a33943db
Gloriel621 <gloriel621@gmail.com>
2021-12-16 17:49:09 +0900

    add get_blocktime

--- crawler.py
+++ crawler.py
@@ -1,24 +1,25 @@
 import requests
+from copy import deepcopy
 from bs4 import BeautifulSoup
+
 from utils import banned_users
 
 
 class Crawler:
     def __init__(self, session: requests.Session, gall_id):
 
-        self.session = session
+        self.session = deepcopy(session)
         self.gall_id = gall_id
         self.BASE_URL = "https://gall.dcinside.com/mgallery/board/lists/"
+        self.management_url = "https://gall.dcinside.com/mgallery/management/gallery"
         self.params = {"id": self.gall_id}
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
         }
 
-    def search_post_nums(self):
+    def get_post_nums(self):
 
-        response = requests.get(
-            self.BASE_URL, params=self.params, headers=self.headers
-        )
+        response = requests.get(self.BASE_URL, params=self.params, headers=self.headers)
         html_data = BeautifulSoup(response.content, "html.parser")
 
         titles = html_data.find("tbody").find_all("tr")
@@ -26,7 +27,6 @@ class Crawler:
         post_num_list = []
 
         for i in titles:
-
             nick = i.find("td", class_="gall_writer ub-writer").text
             post_num = i.find("td", class_="gall_num").text
             banned_user_list = banned_users()
@@ -35,3 +35,21 @@ class Crawler:
                 post_num_list.append(int(post_num))
 
         return post_num_list
+
+    def get_blocktime(self):
+
+        response = self.session.get(self.management_url, params=self.params)
+        if response.status_code != 200:
+            return None
+
+        html_parse = BeautifulSoup(response.text, features="html.parser")
+
+        proxy_txt = html_parse.find("span", class_="proxy_txt").text
+        mobile_txt = html_parse.find("span", class_="mobile_txt").text
+
+        if len(proxy_txt) == 0:
+            proxy_txt = "제한 없음"
+        if len(mobile_txt) == 0:
+            mobile_txt = "제한 없음"
+
+        return [proxy_txt, mobile_txt]
